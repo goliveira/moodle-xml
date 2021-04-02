8771e48d9b37b81a0558ad1b0c6d51efec9b2ebf
Gustavo de Oliveira <goliveira5d@gmail.com>
2021-04-02 12:56:48 -0300

    Example of multichoice question

---
+++ examples/multichoice_1.py
@@ -0,0 +1,27 @@
+import moodlexml as MX
+
+# Create an empty quiz
+quiz = MX.Quiz()
+
+# Create a category with name and append to quiz
+cat = MX.Category("Multichoice questions")
+quiz.append(cat)
+
+# Create a multichoice question with name, text, answers and append to
+# quiz
+name = "<p>Example 1 - The simplest multichoice question</p>"
+text = "<p>What is the area of a triangle of base 7 and height 4?</p>"
+ans1 = ["100", "<p>14</p>"]
+ans2 = ["0", "<p>28</p>"]
+ans3 = ["0", "<p>11</p>"]
+answers = [ans1, ans2, ans3]
+question_mc = MX.Multichoice(name, text, answers)
+quiz.append(question_mc)
+
+# Print quiz to stdout
+print(quiz)
+
+# Write quiz to a file
+text_file = open("output.xml", "w")
+print(quiz, file=text_file)
+text_file.close()
